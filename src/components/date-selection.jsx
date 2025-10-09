408e113b1196f2281c62de663342b0a629763e46
Evandro Tomas <evandrotomas@hotmail.com>
2025-10-08 21:07:09 -0300

    fix: ensure from and to params are only considered if they are valid

--- src/components/date-selection.jsx
+++ src/components/date-selection.jsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { addMonths, format } from 'date-fns'
+import { addMonths, format, isValid } from 'date-fns'
 import { useEffect, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router'
 
@@ -9,20 +9,29 @@ import { DatePickerWithRange } from './ui/date-picker-with-range'
 
 const formatDate = (date) => format(date, 'yyyy-MM-dd')
 
+const getInitialDateState = (searchParams) => {
+  const defaultDate = {
+    from: new Date(),
+    to: addMonths(new Date(), 1),
+  }
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  if (!from || !to) {
+    return defaultDate
+  }
+  const datesAreInvalid = !isValid(new Date(from)) || !isValid(new Date(to))
+  if (datesAreInvalid) {
+    return defaultDate
+  }
+  return defaultDate
+}
+
 const DateSelection = () => {
   const queryClient = useQueryClient()
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
   const { user } = useAuthContext()
-  const [date, setDate] = useState({
-    // quanto eu recarregar a página, eu pego a from e to da url e persisto eles o state
-    from: searchParams.get('from')
-      ? new Date(searchParams.get('from') + 'T00:00:00')
-      : new Date(),
-    to: searchParams.get('to')
-      ? new Date(searchParams.get('to') + 'T00:00:00')
-      : addMonths(new Date(), 1),
-  })
+  const [date, setDate] = useState(getInitialDateState(searchParams))
   // sempre que o state "date" mudar, eu preciso persisti-lo na URL (?from&to=)
   useEffect(() => {
     // early return
