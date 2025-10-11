30f9a544a77bd1e9c4760e01c2c1a278bc4feb41
Evandro Tomas <evandrotomas@hotmail.com>
2025-10-10 21:23:55 -0300

    feat: ensure transaction date and type are rendered properly on table

--- src/components/balance-item.jsx
+++ src/components/balance-item.jsx
@@ -1,3 +1,5 @@
+import { formatCurrency } from '@/helpers/currency'
+
 import { Card, CardContent } from './ui/card'
 
 const BalanceItem = ({ label, icon, value }) => {
@@ -11,12 +13,7 @@ const BalanceItem = ({ label, icon, value }) => {
           </div>
           <p className="text-sm text-muted-foreground">{label}</p>
         </div>
-        <h3 className="text-2xl font-semibold">
-          {new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          }).format(value)}
-        </h3>
+        <h3 className="text-2xl font-semibold">{formatCurrency(value)}</h3>
       </CardContent>
     </Card>
   )
