f1118483ef9192787e3e52a9753dac9553efba18
Evandro Tomas <evandrotomas@hotmail.com>
2025-10-24 16:32:25 -0300

    fix: helper data

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,3 +1,14 @@
 export const getTransactionDate = (transaction) => {
-  return new Date(transaction.date.split('T')[0] + 'T00:00:00')
+  if (!transaction || !transaction.date) return null
+
+  // Extrai apenas a parte da data (AAAA-MM-DD)
+  const [year, month, day] = transaction.date
+    .split('T')[0]
+    .split('-')
+    .map(Number)
+
+  // Cria a data no fuso LOCAL (sem deslocar para UTC)
+  const localDate = new Date(year, month - 1, day, 0, 0, 0, 0)
+
+  return localDate
 }
